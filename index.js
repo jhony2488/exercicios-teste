ba658e5b509b6bc5ffa1eb5574f21f04eafce8ba
Jhonata Vinicius da Silva Araujo <jhon.araujo2488@gmail.com>
2025-03-03 13:30:44 -0300

    add index.js export modules

---
+++ index.js
@@ -0,0 +1,11 @@
+const checkIfTheFirstLetterIsUppercase = require('./utils/exercicio-4')
+const maskify = require('./utils/exercicio-1')
+const updateDataObject = require('./utils/exercicio-2')
+const getRickAndMortyCharacters = require('./utils/exercicio-3')
+
+module.exports = {
+  checkIfTheFirstLetterIsUppercase,
+  getRickAndMortyCharacters,
+  updateDataObject,
+  maskify,
+}
